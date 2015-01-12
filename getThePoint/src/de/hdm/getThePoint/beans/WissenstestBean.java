b835fa389245f801e68d3d26b45ef667a552ffa1
Fabian Röber <fabianroeber@aol.com>
2015-01-12 22:28:44 +0100

    Wissenstestbean

--- getThePoint/src/de/hdm/getThePoint/beans/WissenstestBean.java
+++ getThePoint/src/de/hdm/getThePoint/beans/WissenstestBean.java
@@ -1,10 +1,24 @@
 package de.hdm.getThePoint.beans;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import de.hdm.getThePoint.db.dbmodel.Wissenstest;
 
 @ManagedBean(name = "wissenstestBean")
 @ViewScoped
 public class WissenstestBean {
+
+	private List<Wissenstest> wissenstests;
+
+	public List<Wissenstest> getWissenstests() {
+		return wissenstests;
+	}
+
+	public void setWissenstests(List<Wissenstest> wissenstests) {
+		this.wissenstests = wissenstests;
+	}
 
 }
