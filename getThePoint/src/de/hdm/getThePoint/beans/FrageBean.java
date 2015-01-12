ecc21dd8d16ca7fed2f23dc0da736dec8d9c059e
Fabian Röber <fabianroeber@aol.com>
2015-01-12 20:44:31 +0100

    Wissenstest xhtml und templating

--- getThePoint/src/de/hdm/getThePoint/beans/FrageBean.java
+++ getThePoint/src/de/hdm/getThePoint/beans/FrageBean.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 import de.hdm.getThePoint.bo.AntwortBo;
@@ -19,23 +18,15 @@ import de.hdm.getThePoint.db.mapper.KategorieMapper;
 @ViewScoped
 public class FrageBean implements Serializable {
 
-	String name = "test";
-
 	private static final long serialVersionUID = -7885565449150992040L;
+
+	private List<FrageBo> fragen;
+	private List<KategorieBo> kategorien;
 
 	private FrageMapper frageMapper;
 	private KategorieMapper kategorieMapper;
 
 	private DataAcces dataAccess;
-
-	private List<FrageBo> fragen;
-	private String antwortm1;
-	private String antwortm2;
-	private String antwortm3;
-	private String antwortm4;
-
-	private List<KategorieBo> kategorien;
-	private String bezeichnung;
 
 	public FrageBean() {
 		dataAccess = new DataAcces();
@@ -43,7 +34,6 @@ public class FrageBean implements Serializable {
 		kategorieMapper = new KategorieMapper();
 		getAllFragen();
 		getAllKategorien();
-
 	}
 
 	public void getAllFragen() {
@@ -55,46 +45,6 @@ public class FrageBean implements Serializable {
 		frage.setText("Hier Frage eingeben");
 		frage.setAntwortmoeglichkeiten(new ArrayList<AntwortBo>());
 		fragen.add(frage);
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAntwortm1() {
-		return fragen.get(0).getText();
-	}
-
-	public void setAntwortm1(String antwortm1) {
-		this.antwortm1 = antwortm1;
-	}
-
-	public String getAntwortm2() {
-		return fragen.get(1).getText();
-	}
-
-	public void setAntwortm2(String antwortm2) {
-		this.antwortm2 = antwortm2;
-	}
-
-	public String getAntwortm3() {
-		return fragen.get(2).getText();
-	}
-
-	public void setAntwortm3(String antwortm3) {
-		this.antwortm3 = antwortm3;
-	}
-
-	public String getAntwortm4() {
-		return fragen.get(3).getText();
-	}
-
-	public void setAntwortm4(String antwortm4) {
-		this.antwortm4 = antwortm4;
 	}
 
 	public List<FrageBo> getFragen() {
@@ -116,14 +66,6 @@ public class FrageBean implements Serializable {
 	public void getAllKategorien() {
 		kategorien = kategorieMapper.getModelsAsList(dataAccess
 				.getAllKategorie());
-	}
-
-	public String getBezeichnung() {
-		return bezeichnung;
-	}
-
-	public void setBezeichnung(String bezeichnung) {
-		this.bezeichnung = bezeichnung;
 	}
 
 }
---
+++ getThePoint/src/de/hdm/getThePoint/beans/WissenstestBean.java
@@ -0,0 +1,10 @@
+package de.hdm.getThePoint.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "wissenstestBean")
+@ViewScoped
+public class WissenstestBean {
+
+}
