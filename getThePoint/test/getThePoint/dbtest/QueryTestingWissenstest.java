cccc182fe2aaa8300bf6034cd48038040c6d26ab
Fabian Röber <fabianroeber@aol.com>
2015-01-20 01:16:14 +0100

    Menu

--- getThePoint/test/getThePoint/dbtest/QueryTestingWissenstest.java
+++ getThePoint/test/getThePoint/dbtest/QueryTestingWissenstest.java
@@ -1,10 +1,14 @@
 package getThePoint.dbtest;
+
 import java.util.List;
 
 import org.junit.Test;
 
+import de.hdm.getThePoint.bo.FrageZuordung;
+import de.hdm.getThePoint.bo.WissenstestBo;
 import de.hdm.getThePoint.db.DataAcces;
 import de.hdm.getThePoint.db.dbmodel.Wissenstest;
+import de.hdm.getThePoint.db.mapper.WissenstestMapper;
 
 public class QueryTestingWissenstest {
 
@@ -12,10 +16,11 @@ public class QueryTestingWissenstest {
 	public void test() {
 
 		DataAcces dataAcces = new DataAcces();
+		WissenstestMapper wissenstestMapper = new WissenstestMapper();
 
 		List<Wissenstest> liste = dataAcces.getAllWissentests();
 
-		for (Wissenstest wissenstest: liste) {
+		for (Wissenstest wissenstest : liste) {
 			System.out.println(wissenstest.getBezeichnung());
 			System.out.println(wissenstest.getLehrender().getName());
 		}
